8c21354f22ab9102f65204ed3ef98bb36c34c6ec
Riky Hasibuan <riky.hasibuan@gmail.com>
2020-05-03 14:34:48 +0700

    Double Quote Fixing

--- resources/views/dinasbop/detail.blade.php
+++ resources/views/dinasbop/detail.blade.php
@@ -2,10 +2,10 @@
 @section('content')
     <div id="app">
         <dinasbop-detail 
-            :dinasbop='{!! json_encode($dinasbop) !!}'
-            :dinasboptim='{!! json_encode($dinasboptim) !!}'
-            :route='{!! json_encode($route) !!}'
-            :api='{!! json_encode($api) !!}'>
+            :dinasbop='{{ json_encode($dinasbop) }}'
+            :dinasboptim='{{ json_encode($dinasboptim) }}'
+            :route='{{ json_encode($route) }}'
+            :api='{{ json_encode($api) }}'>
         </dinasbop-detail>
     </div>
 @stop
--- resources/views/dinasregular/detail.blade.php
+++ resources/views/dinasregular/detail.blade.php
@@ -2,11 +2,11 @@
 @section('content')
     <div id="app">
         <dinasregular-detail 
-            :dinasregular='{!! json_encode($dinasregular) !!}'
-            :jenis_transportasi='{!! json_encode($jenistransportasi) !!}'
-            :takaran_liter='{!! json_encode($takaranliter) !!}'
-            :route='{!! json_encode($route) !!}'
-            :api='{!! json_encode($api) !!}'>
+            :dinasregular='{{ json_encode($dinasregular) }}'
+            :jenis_transportasi='{{ json_encode($jenistransportasi) }}'
+            :takaran_liter='{{ json_encode($takaranliter) }}'
+            :route='{{ json_encode($route) }}'
+            :api='{{ json_encode($api) }}'>
         </dinasregular-detail>
     </div>
 @stop
